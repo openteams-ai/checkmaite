65917c2acf6ae1f9ce6e0f30652742d904245c0f
Kim Pevey <kcpevey@quansight.com>
2024-08-29 12:11:48 -0500

    add additional high level abstract classes

--- src/jatic_ri/object_detection/test_stages/interfaces/test_workflows.py
+++ src/jatic_ri/object_detection/test_stages/interfaces/test_workflows.py
@@ -3,8 +3,10 @@
 from jatic_ri._common.test_stages.interfaces.test_stage import TestStage
 from jatic_ri.object_detection.test_stages.interfaces.plugins import (
     MetricThresholdPlugin,
+    MultiModelPlugin,
     SingleDatasetPlugin,
     SingleModelPlugin,
+    TwoDatasetPlugin,
 )
 
 
@@ -24,5 +26,34 @@ class SingleDataset(
     SingleDatasetPlugin,
 ):
     """High level implementation of TestStage requiring one dataset"""
+
+    pass
+
+
+class TwoDataset(
+    TestStage,
+    TwoDatasetPlugin,
+):
+    """High level implementation of TestStage requiring two dataset"""
+
+    pass
+
+
+class MultiModelSingleDataset(
+    TestStage,
+    MultiModelPlugin,
+    SingleDatasetPlugin,
+):
+    """High level implementation of TestStage requiring multiple models and one dataset"""
+
+    pass
+
+
+class DatasetMetricThreshold(
+    TestStage,
+    SingleDatasetPlugin,
+    MetricThresholdPlugin,
+):
+    """High level implementation of TestStage requiring one dataset, metric, and threshold"""
 
     pass
--- tests/test_object_detection/test_test_stages/test_interfaces/test_workflows.py
+++ tests/test_object_detection/test_test_stages/test_interfaces/test_workflows.py
@@ -1,6 +1,35 @@
-from jatic_ri.object_detection.test_stages.interfaces.test_workflows import SingleDataset, ModelDatasetMetricThreshold
+from jatic_ri.object_detection.test_stages.interfaces.test_workflows import SingleDataset, ModelDatasetMetricThreshold, TwoDataset, MultiModelSingleDataset, DatasetMetricThreshold
 from typing import Any
 
+
+def test_model_dataset_metric_threshold(dummy_model, dummy_dataset, dummy_metric) -> None:
+
+    class TestImpl(ModelDatasetMetricThreshold):
+        """Dummy implementation class"""
+        def run(self, use_cache: bool = True) -> None:
+            pass
+
+        def collect_report_consumables(self) -> list[dict[str, Any]]:
+            return super().collect_report_consumables()
+    stage = TestImpl()
+
+    stage.load_model(
+        model=dummy_model,
+        model_id="dummy1",
+        )
+
+    stage.load_dataset(
+        dataset=dummy_dataset,
+        dataset_id="dummy1",
+    )
+
+    stage.load_metric(
+        metric=dummy_metric,
+        metric_id = 'dummy1',
+        )
+
+    dummy_threshold = 99.99
+    stage.load_threshold(threshold = dummy_threshold)
 
 def test_single_dataset(dummy_dataset) -> None:
 
@@ -18,9 +47,49 @@ def test_single_dataset(dummy_dataset) -> None:
         dataset_id="dummy1",
     )
 
-def test_model_dataset_metric_threshold(dummy_dataset) -> None:
+def test_two_dataset(dummy_dataset) -> None:
 
-    class TestImpl(ModelDatasetMetricThreshold):
+    class TestImpl(TwoDataset):
+        """Dummy implementation class"""
+        def run(self, use_cache: bool = True) -> None:
+            pass
+
+        def collect_report_consumables(self) -> list[dict[str, Any]]:
+            return super().collect_report_consumables()
+    stage = TestImpl()
+
+    stage.load_datasets(
+        dataset_1=dummy_dataset,
+        dataset_1_id="dummy1",
+        dataset_2=dummy_dataset,
+        dataset_2_id="dummy2",
+    )
+
+def test_multi_model_single_dataset(dummy_model, dummy_dataset) -> None:
+
+    class TestImpl(MultiModelSingleDataset):
+        """Dummy implementation class"""
+        def run(self, use_cache: bool = True) -> None:
+            pass
+
+        def collect_report_consumables(self) -> list[dict[str, Any]]:
+            return super().collect_report_consumables()
+    stage = TestImpl()
+
+    models = {
+        "dummy1": dummy_model,
+        "dummy2": dummy_model,
+        }
+    stage.load_models(models=models)
+
+    stage.load_dataset(
+        dataset=dummy_dataset,
+        dataset_id="dummy1",
+    )
+
+def test_dataset_metric_threshold(dummy_dataset, dummy_metric) -> None:
+
+    class TestImpl(DatasetMetricThreshold):
         """Dummy implementation class"""
         def run(self, use_cache: bool = True) -> None:
             pass
@@ -33,3 +102,11 @@ def test_model_dataset_metric_threshold(dummy_dataset) -> None:
         dataset=dummy_dataset,
         dataset_id="dummy1",
     )
+
+    stage.load_metric(
+        metric=dummy_metric,
+        metric_id = 'dummy1',
+        )
+
+    dummy_threshold = 99.99
+    stage.load_threshold(threshold = dummy_threshold)
