501c9b0a9debaa4569c61f42648486a97ded791d
Kim Pevey <kcpevey@quansight.com>
2024-10-26 00:07:10 +0000

    Add base panel app

---
+++ src/jatic_ri/object_detection/_panel/configurations/base_app.py
@@ -0,0 +1,176 @@
+"""
+This module contains the BaseApp class, which serves as the base class for all individual configuration pages.
+It provides common methods and visualization elements that can be utilized by the individual pages.
+"""
+
+import panel as pn
+import param
+
+pn.extension("tabulator")
+
+
+class BaseApp(param.Parameterized):
+    """Base class for all the individual configuration pages.
+    This class holds common methods and visualization elements.
+
+    The individual pages should utilize:
+    * a custom title
+    * call `self.view_status_bar` for the status bar visualiation
+      * to change the text on the status bar, use `self.status_text`
+    * call `self.view_title` for the title visualization
+    * apply dropdown/float/int widget input styling like this `pn.widgets.Select.from_param(self.param.model,
+      width=self.widget_width, name='Model', stylesheets=[self.widget_stylesheet])`
+    * apply width to the high level viewable (the outermost element returned from the `panel` method)
+       - width=self.page_width
+    * apply the navy background to the overall page `styles=dict(background=self.color_dark_blue)`
+    * apply the button stylesheets, button_type must be set to "primary"
+    * visualize the export button and implement the _run_export method to run/collect metrics, etc
+    * remove pn.extension from app.py files
+    """
+
+    page_width: int = param.Integer(800)
+    title_font_size: int = param.Integer(default=24)
+    status_text: str = param.String("Waiting for input...")
+    output_test_stages: dict[str, str] = param.Dict({})
+    task: str = param.String("detection")
+    export_button: pn.widgets.Button
+
+    ##################################################
+    # individual apps should override these parameters
+    title: str = param.String(default="Base class title: please update")
+    ##################################################
+
+    def __init__(self, **params: dict[str, object]) -> None:
+        super().__init__(**params)
+        # style guide
+        self.color_light_gray = "#fafefe"
+        self.color_dark_blue = "#1d385b"
+        self.color_light_blue = "#abd1f5"
+        self.color_black = "#000000"
+
+        # this adjusts the margins on the title
+        self.title_stylesheet = """
+:host {
+  --line-height: 10px;
+}
+
+p {
+  padding: 0px;
+  margin: 10px;
+}
+"""
+        self.widget_width = (
+            140  # the widget construct is overriding this via css so we'll specify on the widget object instead
+        )
+        self.widget_height = "20px"
+        self.widget_stylesheet = f"""
+:host {{
+  color: {self.color_light_gray}; /* label text color */
+}}
+
+select:not([multiple]).bk-input, select:not([size]).bk-input {{
+  height: {self.widget_height}; /* dropdown widget height */
+  color: {self.color_black} /* text color on value of Dropdown widgets */
+}}
+
+.bk-input {{
+  height: {self.widget_height};  /* FloatInput widget height */
+  color: {self.color_black} /* text color on value of FloatInput widgets */
+}}
+"""
+        self.button_bgcolor = self.color_light_blue
+        self.button_textcolor = self.color_black
+        self.button_stylesheet = f"""
+:host(.solid) .bk-btn.bk-btn-primary {{
+  background-color: {self.button_bgcolor}
+}}
+
+.bk-btn-primary {{
+  color: {self.button_textcolor}
+}}
+"""
+        self.text_color_styling = f"""
+*, *:before, *:after  {{
+  color: {self.color_light_gray};
+}}
+"""
+        self.info_button_style = f"""
+.bk-description > .bk-icon {{
+  background-color: {self.color_light_gray};
+}}
+"""
+
+        # create export button and connect it to a callback method
+        self.export_button = pn.widgets.Button(
+            name="Export Configuration",
+            button_type="primary",
+            stylesheets=[self.button_stylesheet],
+        )
+        self.export_button.on_click(self.export_button_callback)
+
+        self.output_path = ""
+
+    def _run_export(self) -> None:
+        """Individual implementation of export process.
+        This method should populate the self.output_test_stages with
+        {tool_name: test_stage_dict}. The dictionary you provide should
+        be JSON serializable and should be readable by your individual
+        TestStage class.
+        EVERY TEAM SHOULD OVERWRITE THIS METHOD.
+        """
+        print("THIS SHOULD BE IMPLEMENTED IN THE HIGHER LEVEL CLASS")
+        # for testing and demo purposes only:
+        self.output_test_stages[self.__class__.__name__] = {"TYPE": "base app test"}
+
+    @param.output(output_test_stages=param.Dict)
+    def _output(self) -> dict:
+        return self.output_test_stages
+
+    @param.output(task=param.String)
+    def _task(self) -> str:  # pragma: no cover
+        return self.task
+
+    def export_button_callback(self, _event: object) -> None:
+        """Method to take the configuration and save to disk.
+        DO NOT OVERWRITE THIS METHOD.
+        """
+        self._run_export()
+        self.status_text = "Configuration saved"
+
+    def view_status_bar(self) -> pn.Column:
+        """View of status bar. Change the text on the status bar by modifying
+        the `self.status_text` variable.
+        DO NOT OVERWRITE THIS METHOD
+        """
+        return pn.Column(
+            pn.layout.Divider(),
+            pn.Row(
+                pn.pane.Markdown("Status:", styles={"color": f"{self.color_light_gray}"}),
+                pn.pane.Markdown(
+                    self.status_text,
+                    sizing_mode="stretch_width",
+                    styles={"color": f"{self.color_light_gray}"},
+                ),
+            ),
+        )
+
+    def view_title(self) -> pn.pane.Markdown:
+        """View of the app title
+        DO NOT OVERWRITE THIS METHOD
+        """
+        return pn.pane.Markdown(
+            self.title,
+            styles={"font-size": f"{self.title_font_size}px", "color": f"{self.color_light_gray}"},
+            stylesheets=[self.title_stylesheet],
+        )
+
+    def panel(self) -> pn.Column:
+        """Example usage of this base class
+        EVERY TEAM SHOULD  OVERWRITE THIS METHOD
+        """
+        return pn.Column(
+            self.view_title,
+            self.view_status_bar,
+            width=self.page_width,
+            styles={"background": self.color_dark_blue},
+        )
---
+++ tests/test_object_detection/test_panel/test_base_app.py
@@ -0,0 +1,25 @@
+"""Test base app"""
+
+from jatic_ri.object_detection._panel.configurations.base_app import BaseApp
+
+
+def test_base_app_widgets() -> None:
+    """This tests the basic functionality provided in the base class"""
+    # instantiate the panel app
+    base_app = BaseApp()
+    # run through visualization
+    # it can't be viewed this way, but it will allow us to catch some errors
+    base_app.panel()
+
+    initial_status_text = base_app.status_text
+    # click the export button
+    #    this calls `export_button_callback` which then calls
+    #    `_run_export` and updates the `status_text`
+    base_app.export_button.clicks += 1
+    # ensure _run_export is called
+    assert base_app.__class__.__name__ in base_app.output_test_stages
+    # ensure status text changed
+    assert base_app.status_text != initial_status_text
+
+    # test the panel stage output
+    assert base_app.output_test_stages == base_app._output()
