31436830ddbbf1d2ab78e92b635a1148e169bef1
Marco Gorelli <mgorelli@quansight.com>
2024-12-03 18:54:44 +0000

    feat: Add TorchvisionICModel

--- src/jatic_ri/image_classification/models.py
+++ src/jatic_ri/image_classification/models.py
@@ -7,7 +7,7 @@ import json
 from typing import TYPE_CHECKING, Any, Optional
 
 import torch
-from maite.protocols import object_detection as od
+from maite.protocols import image_classification as ic
 
 from jatic_ri._common.models import (
     InvalidModelNameError,
@@ -21,30 +21,19 @@ from jatic_ri._common.models import (
     to_torch_batch,
     validate_input_batch,
 )
-from jatic_ri.object_detection.datasets import DetectionTarget
 
 if TYPE_CHECKING:
     from torch import nn
 
 SUPPORTED_TORCHVISION_MODELS = {
-    "fasterrcnn_resnet50_fpn": "FasterRCNN_ResNet50_FPN_Weights",
-    "fasterrcnn_resnet50_fpn_v2": "FasterRCNN_ResNet50_FPN_V2_Weights",
-    "fasterrcnn_mobilenet_v3_large_fpn": "FasterRCNN_MobileNet_V3_Large_FPN_Weights",
-    "fasterrcnn_mobilenet_v3_large_320_fpn": "FasterRCNN_MobileNet_V3_Large_320_FPN_Weights",
-    "maskrcnn_resnet50_fpn_v2": "MaskRCNN_ResNet50_FPN_V2_Weights",
-    "maskrcnn_resnet50_fpn": "MaskRCNN_ResNet50_FPN_Weights",
-    "retinanet_resnet50_fpn_v2": "RetinaNet_ResNet50_FPN_V2_Weights",
-    "retinanet_resnet50_fpn": "RetinaNet_ResNet50_FPN_Weights",
-    "fcos_resnet50_fpn": "FCOS_ResNet50_FPN_Weights",
-    "keypointrcnn_resnet50_fpn": "KeypointRCNN_ResNet50_FPN_Weights",
-    "ssd300_vgg16": "SSD300_VGG16_Weights",
-    "ssdlite320_mobilenet_v3_large": "SSDLite320_MobileNet_V3_Large_Weights",
+    "alexnet": "AlexNet_Weights",
+    "resnext50_32x4d": "ResNeXt50_32X4D_Weights",
 }
 
 
-class TorchvisionODModel:
+class TorchvisionICModel:
     """
-    A MAITE-compliant wrapper for object detection models from the torchvision package.
+    A MAITE-compliant wrapper for image classification models from the torchvision package.
 
     The primary purpose of this wrapper is to ensure that the model is called correctly.
     This includes:
@@ -55,7 +44,7 @@ class TorchvisionODModel:
           for inference, and similarly when predictions are returned by the model.
 
     Attributes:
-        name: Name of the object detection model (e.g., 'fasterrcnn_resnet50_fpn').
+        name: Name of the image classification model (e.g., 'alexnet').
         device: Device where the model will run ('cpu' or 'cuda').
         preprocess: Preprocessing transforms necessary for converting images to the format
           expected by the model.
@@ -88,9 +77,9 @@ class TorchvisionODModel:
         self._model_name = model_name
 
         try:
-            model = getattr(importlib.import_module("torchvision.models.detection"), model_name)
+            model = getattr(importlib.import_module("torchvision.models"), model_name)
             torchvision_weights_constructor = getattr(
-                importlib.import_module("torchvision.models.detection"), SUPPORTED_TORCHVISION_MODELS[model_name]
+                importlib.import_module("torchvision.models"), SUPPORTED_TORCHVISION_MODELS[model_name]
             )
         except Exception as e:
             raise TorchvisionImportError(f"There was an error importing {model_name} from torchvision.") from e
@@ -109,30 +98,25 @@ class TorchvisionODModel:
             except FileNotFoundError:
                 raise MissingConfigFileError(f"Configuration file not found at path: {config_path}") from None
 
-            num_classes = model_config.get("num_classes", None)
-
             self.index2label = get_index2label_from_model_config(config_path, model_config, index2label_key)
+            num_classes = model_config.get("num_classes", len(self.index2label))
 
             # initializing model with random weights and then loading weights via state_dict
             # appears to be easiest method for allowing user to bring their own weights.
-            # gotcha: it's not sufficient to set weights to None, the weights for the backbone
-            # model must also be set to None, otherwise torchvision will instantiate a subtly
-            # different model architecture in some cases
-            self.model = model(weights=None, weights_backbone=None, num_classes=num_classes, **kwargs).to(self.device)
+            self.model = model(weights=None, num_classes=num_classes, **kwargs).to(self.device)
 
             try:
                 state_dict = torch.load(weights_path, map_location=torch.device(self.device), weights_only=True)
                 self.model.load_state_dict(state_dict)
             except Exception as e:
                 raise StateDictLoadError(f"Error loading data from state_dict from {weights_path}") from e
-
         else:  # we are loading torchvision pre-trained weights into model
             self.index2label = get_default_index2label(torchvision_weights_constructor)
             self.model = maybe_download_weights(model, torchvision_weights_constructor, self.device, **kwargs)
 
         self.model.eval()  # sets model to inference mode rather than training mode
 
-    def __call__(self, input_batch: od.InputBatchType) -> od.TargetBatchType:
+    def __call__(self, input_batch: ic.InputBatchType) -> ic.TargetBatchType:
         """
         Make a model prediction for inputs in input batch. Elements of input batch
         are expected in the shape `(C, H, W)`.
@@ -140,21 +124,11 @@ class TorchvisionODModel:
         validate_input_batch(input_batch)
         torch_batch = to_torch_batch(input_batch, self.device)
         batch_on_device = self.preprocess(torch_batch).to(self.device)
-        predictions_batch = self.model(batch_on_device)
 
-        # gotcha: torchvision boxes do not need to be denormalised post inference!
-        # see https://github.com/pytorch/vision/issues/2397
-        # hence can just return predictions directly
-        return [
-            DetectionTarget(
-                boxes=pred["boxes"]
-                .detach()
-                .cpu(),  # transfer to CPU as downstream package may not support other devices
-                labels=pred["labels"].detach().cpu(),
-                scores=pred["scores"].detach().cpu(),
-            )
-            for pred in predictions_batch
-        ]
+        with torch.no_grad():
+            logits_batch = self.model(batch_on_device)
+            # transfer to CPU as downstream package may not support other devices
+            return list(torch.nn.functional.softmax(input=logits_batch, dim=1).cpu().detach())
 
     @property
     def name(self) -> str:
